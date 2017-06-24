ae830189cae182f602e474c3b32e1a14c732762e
Nate Clark <nate@hired.com>
2017-06-24 13:15:31 -0700

    code formatting

--- source-code/application.lua
+++ source-code/application.lua
@@ -1,34 +1,34 @@
-local sensorSend = { }
+local sensorSend = {}
 
-for i,sensor in pairs(sensors) do
+for i, sensor in pairs(sensors) do
   print('Initializing sensor on pin ' .. sensor.pin)
   gpio.mode(sensor.pin, gpio.INPUT, gpio.PULLUP)
 end
 
-for i,actuator in pairs(actuators) do
+for i, actuator in pairs(actuators) do
   print('Initializing actuator on pin ' .. actuator.pin)
   gpio.mode(actuator.pin, gpio.OUTPUT)
   gpio.write(actuator.pin, gpio.LOW)
 end
 
-tmr.create():alarm(200, tmr.ALARM_AUTO,function(t)
-	for i, sensor in pairs(sensors) do
-		if sensor.state ~= gpio.read(sensor.pin) then
-			sensor.state = gpio.read(sensor.pin)
-			table.insert(sensorSend, i)
-		end		
-	end
+tmr.create():alarm(200, tmr.ALARM_AUTO, function(t)
+  for i, sensor in pairs(sensors) do
+    if sensor.state ~= gpio.read(sensor.pin) then
+      sensor.state = gpio.read(sensor.pin)
+      table.insert(sensorSend, i)
+    end
+  end
 end)
 
 tmr.create():alarm(200, tmr.ALARM_AUTO, function(t)
-	if sensorSend[1] then
-		t:stop()
-		local sensor = sensors[sensorSend[1]]
-		http.put(smartthings.apiUrl.."\/device\/"..wifi.sta.getmac():gsub("%:", "").."\/"..sensor.pin.."\/"..gpio.read(sensor.pin), "Authorization: Bearer "..smartthings.token.."\r\n", "", function(stat, b, h) 
-			table.remove(sensorSend, 1)
-			t:start()
-		end)
-	end
+  if sensorSend[1] then
+    t:stop()
+    local sensor = sensors[sensorSend[1]]
+    http.put(smartthings.apiUrl .. "\/device\/" .. wifi.sta.getmac():gsub("%:", "") .. "\/" .. sensor.pin .. "\/" .. gpio.read(sensor.pin), "Authorization: Bearer " .. smartthings.token .. "\r\n", "", function(stat, b, h)
+      table.remove(sensorSend, 1)
+      t:start()
+    end)
+  end
 end)
 
 tmr.create():alarm(5000, tmr.ALARM_AUTO, function()
--- source-code/init.lua
+++ source-code/init.lua
@@ -1,23 +1,23 @@
-print("Starting.. Memory: "..node.heap())
+print("Starting.. Memory: " .. node.heap())
 require("variables")
 enduser_setup.manual(false)
 enduser_setup.start()
 gpio.mode(4, gpio.OUTPUT)
 tmr.create():alarm(700, tmr.ALARM_AUTO, function(t)
-	if gpio.read(4) == gpio.LOW then
-		gpio.write(4, gpio.HIGH)
-	else
-		gpio.write(4, gpio.LOW)
-	end    
-	if wifi.sta.getip() then
-		t:unregister()
-		t = nil       
-		gpio.write(4, gpio.LOW)
-		enduser_setup.stop()
-		require("server")   
-		require("application")		
-		print("Started.. Memory: "..node.heap().." IP: "..wifi.sta.getip())
-	end
+  if gpio.read(4) == gpio.LOW then
+    gpio.write(4, gpio.HIGH)
+  else
+    gpio.write(4, gpio.LOW)
+  end
+  if wifi.sta.getip() then
+    t:unregister()
+    t = nil
+    gpio.write(4, gpio.LOW)
+    enduser_setup.stop()
+    require("server")
+    require("application")
+    print("Started.. Memory: " .. node.heap() .. " IP: " .. wifi.sta.getip())
+  end
 end)
 
 
