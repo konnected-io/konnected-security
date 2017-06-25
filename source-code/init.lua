9f369745ac2aba27eb5de21b2ee5b86558e19500
copy-ninja <jason@copyninja.net>
2017-06-24 22:56:11 -0500

    added flag to check if to call enduser setup
    -added flag to check if to call enduser setup. It should optimize the memory even further

--- source-code/init.lua
+++ source-code/init.lua
@@ -1,10 +1,14 @@
-print("Starting.. Memory: " .. node.heap())
+print("Heap: ", node.heap(), "Initializing device")
 require("compile")
+print("Heap: ", node.heap(), "Loaded: ", "compile")
 require("variables")
 print("Heap: ", node.heap(), "Loaded: ", "variables")
 
-enduser_setup.manual(false)
-enduser_setup.start()
+if wifi.sta.getconfig() == "" then 
+  enduser_setup.manual(false)
+  enduser_setup.start()
+  print("Heap: ", node.heap(), "End User Setup started")
+end
 
 tmr.create():alarm(700, tmr.ALARM_AUTO, function(t)
   if gpio.read(4) == gpio.LOW then
@@ -15,12 +19,12 @@ tmr.create():alarm(700, tmr.ALARM_AUTO, function(t)
   if wifi.sta.getip() then
     t:unregister()
     t = nil
+    print("Heap: ", node.heap(), "Wifi connected with IP: ", wifi.sta.getip())
     gpio.write(4, gpio.HIGH)
     enduser_setup.stop()
     require("server")
     print("Heap: ", node.heap(), "Loaded: ", "server")
     require("application")
     print("Heap: ", node.heap(), "Loaded: ", "application")
-    print("Started.. Memory: " .. node.heap() .. " IP: " .. wifi.sta.getip())
   end
 end)
