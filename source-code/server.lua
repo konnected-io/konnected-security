2884de9da7ae3b51e7da139f0df8f3d9429985a2
copy-ninja <jason@copyninja.net>
2017-06-26 22:40:47 -0500

    Added /update on http server

--- source-code/server.lua
+++ source-code/server.lua
@@ -3,6 +3,18 @@ print("Heap: ", node.heap(), "Loaded: ", "httpd")
 require("ssdp")
 print("Heap: ", node.heap(), "Loaded: ", "ssdp")
 
+local function variables_set(name, value)
+  local fnc = string.match(name, ".*%.")
+  local fn = "var_" .. name .. '.lua'
+  local f = file.open(fn, "w")
+  f.writeline(name .. " = " .. value)
+  f.close()
+  node.compile(fn)
+  file.remove(fn)
+  print("Heap: ", node.heap(), "Wrote: ", fn)
+  collectgarbage()
+end
+
 httpd_set("/", function(request, response)
   response:file("http_index.html")
 end)
@@ -13,6 +25,7 @@ httpd_set("/favicon.ico", function(request, response)
 end)
 
 httpd_set("/settings", function(request, response)
+  print("Heap: ", node.heap(), "HTTP: ", "Settings")
   local function buildValue(objects)
     local out = {}
     table.insert(out, "{ ")
@@ -30,17 +43,7 @@ httpd_set("/settings", function(request, response)
 
   if request.contentType == "application/json" then
     if request.method == "PUT" then
-      local function variables_set(name, value)
-        local fnc = string.match(name, ".*%.")
-        local fn = "var_" .. name .. '.lua'
-        local f = file.open(fn, "w")
-        f.writeline(name .. " = " .. value)
-        f.close()
-        node.compile(fn)
-        file.remove(fn)
-        print("Heap: ", node.heap(), "Wrote: ", fn)
-        collectgarbage()
-      end
+      
       variables_set("smartthings", table.concat({ "{ token = \"", request.body.token, "\",\r\n apiUrl = \"", request.body.apiUrl, "\" }" }))
       variables_set("sensors", buildValue(request.body.sensors))
       variables_set("actuators", buildValue(request.body.actuators))
@@ -56,6 +59,7 @@ httpd_set("/settings", function(request, response)
 end)
 
 httpd_set("/device", function(request, response)
+  print("Heap: ", node.heap(), "HTTP: ", "Device")
   if request.contentType == "application/json" then
     if request.method == "GET" then
       local body = {
@@ -76,6 +80,7 @@ httpd_set("/device", function(request, response)
 end)
 
 httpd_set("/status", function(request, response)
+  print("Heap: ", node.heap(), "HTTP: ", "Status")
   local body = {
     hwVersion = device.name .. " \/ " .. device.hwVersion,
     swVersion = device.swVersion,
@@ -89,7 +94,17 @@ httpd_set("/status", function(request, response)
 end)
 
 httpd_set("/restart", function(request, response)
-  tmr.create():alarm(5000, tmr.ALARM_SINGLE, node.restart)
+  print("Heap: ", node.heap(), "HTTP: ", "Restart")
+  tmr.create():alarm(5000, tmr.ALARM_SINGLE, function() node.restart() end)
+  response:contentType("application/json")
+  response:status("204")
+  response:send("")
+end)
+
+httpd_set("/update", function(request, response)
+  print("Heap: ", node.heap(), "HTTP: ", "Update")
+  variables_set("update", "true")
+  tmr.create():alarm(5000, tmr.ALARM_SINGLE, function() node.restart() end)
   response:contentType("application/json")
   response:status("204")
   response:send("")
