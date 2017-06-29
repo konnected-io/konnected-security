d1fee72daa41aa5a26f5f38966a1209c6dfb196b
copy-ninja <jason@copyninja.net>
2017-06-28 22:03:54 -0500

    no longer use var_ file system
    remove load variables

--- source-code/variables.lua
+++ source-code/variables.lua
@@ -1,13 +1,5 @@
 gpio.mode(4, gpio.OUTPUT)
 
-for fn in pairs(file.list()) do
-  local fm = string.match(fn,"var_.*")
-  if (fm) then 
-    dofile(fm) 
-    print("Heap: ", node.heap(), "Loaded: ", fn)
-  end
-end
-
 blinktimer = tmr.create()
 blinktimer:register(100, tmr.ALARM_SEMI, function(t)
   if gpio.read(4) == gpio.HIGH then
