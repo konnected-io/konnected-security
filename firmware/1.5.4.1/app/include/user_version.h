26c32dab5f37e16ed2476a80f276498fe7ed23ba
copy-ninja <jason@copyninja.net>
2017-07-12 22:06:04 -0500

    dropping beta tag
    firmware will probably not change until we get a stable NodeMCU firmware. we probably will not encourage this to change at all. There will be a lot of rewriting if we change to new NodeMCU firmware.

--- firmware/1.5.4.1/app/include/user_version.h
+++ firmware/1.5.4.1/app/include/user_version.h
@@ -6,7 +6,7 @@
 #define NODE_VERSION_REVISION	4U
 #define NODE_VERSION_INTERNAL   1U
 
-#define NODE_VERSION	"NodeMCU 1.5.4.1\r\nKonnected Security 2.0.beta1\r\n"
+#define NODE_VERSION	"NodeMCU 1.5.4.1\r\nKonnected Security 2.0\r\n"
 #ifndef BUILD_DATE
 #define BUILD_DATE	  "20170708"
 #endif
