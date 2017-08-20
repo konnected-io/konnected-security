a835002872573537947944baf6ebc0c70f877090
Nate Clark <nate@nateclark.com>
2017-08-19 20:39:50 -0700

    when ip address changes, properly report updated IP address on discovery

--- spec/device_spec.lua
+++ spec/device_spec.lua
@@ -2,11 +2,12 @@ describe("device", function()
   local device
 
   setup(function()
+    require("spec/nodemcu_stubs")
     device = require("device")
   end)
 
   it("has expected properties", function()
-    assert.equal(device.swVersion, "2.0.2")
+    assert.equal(device.swVersion, "2.0.4")
     assert.equal(device.hwVersion, "2.0.0")
     assert.equal(device.name, "Security")
   end)
