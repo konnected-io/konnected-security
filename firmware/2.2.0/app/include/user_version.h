89717e655cc9a77554edf1748a403c80717e8173
Nate Clark <nate@nateclark.com>
2018-04-05 12:04:27 -0400

    first build on NodeMCU 2.2

---
+++ firmware/2.2.0/app/include/user_version.h
@@ -0,0 +1,22 @@
+#ifndef __USER_VERSION_H__
+#define __USER_VERSION_H__
+
+#include "version.h"	/* ESP firmware header */
+
+#define NODE_VERSION_MAJOR	ESP_SDK_VERSION_MAJOR
+#define NODE_VERSION_MINOR	ESP_SDK_VERSION_MINOR
+#define NODE_VERSION_REVISION	ESP_SDK_VERSION_PATCH
+#define NODE_VERSION_INTERNAL   0
+
+#define NODE_VERSION_STR(x)	#x
+#define NODE_VERSION_XSTR(x)	NODE_VERSION_STR(x)
+
+#define NODE_VERSION		"Konnected firmware 2.2\r\nNodeMCU " ESP_SDK_VERSION_STRING "." NODE_VERSION_XSTR(NODE_VERSION_INTERNAL)
+
+#ifndef BUILD_DATE
+#define BUILD_DATE		"20180405"
+#endif
+
+extern char SDK_VERSION[];
+
+#endif	/* __USER_VERSION_H__ */
