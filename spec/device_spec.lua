35830287a7e84e17410fbb82e12c1bed7c5df51f
Nate Clark <nate@nateclark.com>
2019-01-14 13:14:42 -0500

    build firmware/fs 2.2.5

--- spec/device_spec.lua
+++ spec/device_spec.lua
@@ -7,8 +7,8 @@ describe("device", function()
   end)
 
   it("has expected properties", function()
-    assert.equal(device.swVersion, "2.2.4")
-    assert.equal(device.hwVersion, "2.2.4")
+    assert.equal(device.swVersion, "2.2.5.beta1")
+    assert.equal(device.hwVersion, "2.2.5")
     assert.equal(device.name, "Konnected")
   end)
 
