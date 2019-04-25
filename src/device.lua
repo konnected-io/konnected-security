16da276038756fd697e375d53e475a18603f7269
Nate Clark <nate@nateclark.com>
2019-04-24 23:15:36 -0400

    customize lwipopts in firmware build

--- src/device.lua
+++ src/device.lua
@@ -1,7 +1,7 @@
 local me = {
   id = "uuid:8f655392-a778-4fee-97b9-4825918" .. string.format("%x", node.chipid()),
   name = "Konnected",
-  hwVersion = "2.2.6",
+  hwVersion = "2.2.7.beta1",
   swVersion = "2.2.6",
   http_port = math.floor(node.chipid()/1000) + 8000,
   urn = "urn:schemas-konnected-io:device:Security:1"
