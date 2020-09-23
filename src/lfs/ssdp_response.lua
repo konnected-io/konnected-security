c02c00b2bb5b2975641f592c29537590957d6459
Kit Klein <kit@kleinlabs.org>
2020-09-22 20:11:28 -0400

    tweaks based on integration testing

--- src/lfs/ssdp_response.lua
+++ src/lfs/ssdp_response.lua
@@ -3,6 +3,7 @@ local module = ...
 local function ssdpResponse(c, d, port, ip)
   if string.match(d, "M-SEARCH") then
     local device = require("device")
+    local info = node.info("sw_version")
     local urn = d:match("ST: (urn:[%w%p]*)")
     if (urn == device.urn or string.match(d, "ST: ssdp:all")) then
       print("Heap: ", node.heap(), "Responding to UPnP Discovery request from " .. ip .. ":" .. port)
@@ -11,7 +12,7 @@ local function ssdpResponse(c, d, port, ip)
         "CACHE-CONTROL: max-age=1800\r\n" ..
         "ST: " .. device.urn .. "\r\n" ..
         "USN: " .. device.id .. "::" .. device.urn .. "\r\n" .. "EXT:\r\n" ..
-        "SERVER: NodeMCU/" .. string.format("%d.%d.%d", node.info()) .. " UPnP/1.1 " .. device.name .. "/" .. device.swVersion .. "\r\n" ..
+        "SERVER: NodeMCU/" .. string.format("%d.%d.%d", info.node_version_major, info.node_version_minor, info.node_version_revision) .. " UPnP/1.1 " .. device.name .. "/" .. device.swVersion .. "\r\n" ..
         "LOCATION: http://" .. wifi.sta.getip() .. ":" .. device.http_port .. "/Device.xml\r\n\r\n"
       c:send(port, ip, resp)
       resp = nil
